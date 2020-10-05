5acdfa0a859647ef0556e4691dc0c3d35bfc268f
Ben Cooper <bccooper1996@gmail.com>
2020-10-04 20:56:19 -0700

    Add 2 column ui

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -64,16 +64,47 @@ namespace Hazel {
 
 	void SceneHierarchyPanel::DrawComponents(Entity entity)
 	{
+		/*
+			To use ImGui::BeginColumns() and ImGui::EndColumns(), move these from imgui_internal.h to imgui.h:
+			`typedef int ImGuiColumnsFlags;      // -> enum ImGuiColumnsFlags_       // Flags: BeginColumns()`
+
+			`IMGUI_API void          BeginColumns(const char* str_id, int count, ImGuiColumnsFlags flags = 0); // setup number of columns. use an identifier to distinguish multiple column sets. close with EndColumns().`
+			`IMGUI_API void          EndColumns();                                                             // close columns`
+
+			```
+			enum ImGuiColumnsFlags_
+			{
+				// Default: 0
+				ImGuiColumnsFlags_None = 0,
+				ImGuiColumnsFlags_NoBorder = 1 << 0,   // Disable column dividers
+				ImGuiColumnsFlags_NoResize = 1 << 1,   // Disable resizing columns when clicking on the dividers
+				ImGuiColumnsFlags_NoPreserveWidths = 1 << 2,   // Disable column width preservation when adjusting columns
+				ImGuiColumnsFlags_NoForceWithinWindow = 1 << 3,   // Disable forcing columns to fit within window
+				ImGuiColumnsFlags_GrowParentContentsSize = 1 << 4    // (WIP) Restore pre-1.51 behavior of extending the parent window contents size but _without affecting the columns width at all_. Will eventually remove.
+			};
+			```
+		*/
+
 		if (entity.HasComponent<TagComponent>())
 		{
-			auto& tag = entity.GetComponent<TagComponent>().Tag;
-
-			char buffer[256];
-			memset(buffer, 0, sizeof(buffer));
-			strcpy_s(buffer, sizeof(buffer), tag.c_str());
-			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			if (ImGui::TreeNodeEx((void*)typeid(TagComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Tag"))
 			{
-				tag = std::string(buffer);
+				ImGui::BeginColumns("Tag", 2, 0);
+
+				auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+				char buffer[256];
+				memset(buffer, 0, sizeof(buffer));
+				strcpy_s(buffer, sizeof(buffer), tag.c_str());
+				ImGui::Text("Tag");
+				ImGui::NextColumn();
+				if (ImGui::InputText("##Tag", buffer, sizeof(buffer)))
+				{
+					tag = std::string(buffer);
+				}
+
+				ImGui::EndColumns();
+				ImGui::TreePop();
 			}
 		}
 
@@ -81,9 +112,14 @@ namespace Hazel {
 		{
 			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
 			{
-				auto& transform = entity.GetComponent<TransformComponent>().Transform;
-				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+				ImGui::BeginColumns("Transform", 2, 0);
 
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::Text("Position");
+				ImGui::NextColumn();
+				ImGui::DragFloat3("##Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::EndColumns();
 				ImGui::TreePop();
 			}
 		}
@@ -92,14 +128,20 @@ namespace Hazel {
 		{
 			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
 			{
+				ImGui::BeginColumns("Camera", 2, 0);
 				auto& cameraComponent = entity.GetComponent<CameraComponent>();
 				auto& camera = cameraComponent.Camera;
 
-				ImGui::Checkbox("Primary", &cameraComponent.Primary);
+				ImGui::Text("Primary");
+				ImGui::NextColumn();
+				ImGui::Checkbox("##Primary", &cameraComponent.Primary);
 
 				const char* projectionTypeStrings[] = { "Perspective", "Orthographic" };
 				const char* currentProjectionTypeString = projectionTypeStrings[(int)camera.GetProjectionType()];
-				if (ImGui::BeginCombo("Projection", currentProjectionTypeString))
+				ImGui::NextColumn();
+				ImGui::Text("Projection");
+				ImGui::NextColumn();
+				if (ImGui::BeginCombo("##Projection", currentProjectionTypeString))
 				{
 					for (int i = 0; i < 2; i++)
 					{
@@ -120,36 +162,57 @@ namespace Hazel {
 				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Perspective)
 				{
 					float verticalFov = glm::degrees(camera.GetPerspectiveVerticalFOV());
-					if (ImGui::DragFloat("Vertical FOV", &verticalFov))
+					ImGui::NextColumn();
+					ImGui::Text("Vertical FOV");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Vertical FOV", &verticalFov))
 						camera.SetPerspectiveVerticalFOV(glm::radians(verticalFov));
 
 					float orthoNear = camera.GetPerspectiveNearClip();
-					if (ImGui::DragFloat("Near", &orthoNear))
+					ImGui::NextColumn();
+					ImGui::Text("Near");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Near", &orthoNear))
 						camera.SetPerspectiveNearClip(orthoNear);
 
 					float orthoFar = camera.GetPerspectiveFarClip();
-					if (ImGui::DragFloat("Far", &orthoFar))
+					ImGui::NextColumn();
+					ImGui::Text("Far");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Far", &orthoFar))
 						camera.SetPerspectiveFarClip(orthoFar);
 				}
 
 				if (camera.GetProjectionType() == SceneCamera::ProjectionType::Orthographic)
 				{
 					float orthoSize = camera.GetOrthographicSize();
-					if (ImGui::DragFloat("Size", &orthoSize))
+					ImGui::NextColumn();
+					ImGui::Text("Size");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Size", &orthoSize))
 						camera.SetOrthographicSize(orthoSize);
 
 					float orthoNear = camera.GetOrthographicNearClip();
-					if (ImGui::DragFloat("Near", &orthoNear))
+					ImGui::NextColumn();
+					ImGui::Text("Near");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Near", &orthoNear))
 						camera.SetOrthographicNearClip(orthoNear);
 
 					float orthoFar = camera.GetOrthographicFarClip();
-					if (ImGui::DragFloat("Far", &orthoFar))
+					ImGui::NextColumn();
+					ImGui::Text("Far");
+					ImGui::NextColumn();
+					if (ImGui::DragFloat("##Far", &orthoFar))
 						camera.SetOrthographicFarClip(orthoFar);
 
-					ImGui::Checkbox("Fixed Aspect Ratio", &cameraComponent.FixedAspectRatio);
+					ImGui::NextColumn();
+					ImGui::Text("Fixed Aspect Ratio");
+					ImGui::NextColumn();
+					ImGui::Checkbox("##Fixed Aspect Ratio", &cameraComponent.FixedAspectRatio);
 				}
 
-
+				ImGui::EndColumns();
 				ImGui::TreePop();
 			}
 		}
